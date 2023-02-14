366c9a9ce9d80e18bc6c051b6bd1dbc33139f29e
Wiqzard <s.stapf@aol.com>
2023-02-14 17:58:49 +0100

    refactor

--- tracking/rack_counter.py
+++ tracking/rack_counter.py
@@ -339,7 +339,7 @@ class ScannerCounterAnnotator:
                 start=rack_scanner.scanner.start,
                 height=rack_scanner.scanner.height,
             )
-            frame = self.draw_counter(
+            self.draw_counter(
                 scene=frame,
                 class_id=rack_scanner.curr_rack,
                 rack_tracker=rack_scanner.rack_tracks[-1],
