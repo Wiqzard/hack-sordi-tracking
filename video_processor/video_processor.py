02cb0fa9ad19e531e39c5e8cfa23a0f689c7966b
Wiqzard <s.stapf@aol.com>
2023-02-14 18:34:39 +0100

    refactor

--- video_processor/video_processor.py
+++ video_processor/video_processor.py
@@ -388,8 +388,8 @@ class VideoProcessor:
 
                     # sort the frames depending on intital batch index
                     print(list(frames_gen))
-                    frames_ordered = sorted(list(frames_gen), key=lambda x: x[0])
-                    frames_ordered = [x[1] for x in frames_ordered]
+                    frames_ordered = sorted(list(frames_gen), key=lambda x: x[1])
+                    frames_ordered = [x[0] for x in frames_ordered]
                     for frame in frames_ordered:
                         sink.write_frame(frame)
 
