c82e8cfdcca17835fe487d2fa505e535d9e15e77
Wiqzard <s.stapf@aol.com>
2023-02-14 22:49:32 +0100

    refactor

--- video_processor/video_processor.py
+++ video_processor/video_processor.py
@@ -402,10 +402,13 @@ class VideoProcessor:
                     # frames_ordered = sorted(list(frames_gen), key=lambda x: x[1])
                     # frames_ordered = [x[0] for x in frames_ordered]
                     # print(dict(frames_gen))
-                    for idx, frames_dict in zip(range(len(batch)), dict(frames_gen)):
-                        print(frames_dict[idx])
+                    frames_dict = dict(frames_gen)
+                    for idx in range(len(frames_dict)):
                         sink.write_frame(frames_dict[idx])
-                        # sink.write_frame(frame)
+                    # for idx, frames_dict in zip(range(len(batch)), dict(frames_gen)):
+                    #    print(frames_dict[idx])
+                    #    sink.write_frame(frames_dict[idx])
+                    # sink.write_frame(frame)
                     # for idx, frame in frames_ordered:
                     #    print(frame.shape)
                     #    sink.write_frame(frame)
